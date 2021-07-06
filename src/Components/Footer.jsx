01b980b53b56aca75fa5b3bba39a5f39f937fd98
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-07-06 16:04:37 +0200

    completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css"
+
+const Footer = () => (
+    <footer className="Footer">
+      <p>Keep Eating!</p>
+    </footer>
+  );
+
+  export default Footer;
---
+++ src/Components/Title.jsx
@@ -0,0 +1,9 @@
+import "./Title.css"
+
+function Title() {
+  return <div className="Title">
+  <h1 className="TitleA">Blueberry Pancakes!</h1>
+  </div>;
+};
+
+export default Title;
